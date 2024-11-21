d143dcf59f83dd6b6b5df97c8b6c4239e56441ff
tankmurdock <new.support@gmail.com>
2024-11-21 09:41:01 -0500

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // Or if using `src` directory:
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/slices/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["--font-dm-sans"],
+      },
+    },
+  },
+
+  plugins: [require("@tailwindcss/typography")],
+};
